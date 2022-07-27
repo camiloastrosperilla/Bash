074c5151797d191459f4302af0140d49ec2a2113
camiloastrosperilla <camiloastros@gmail.com>
2022-07-26 20:47:29 -0500

    Scripting Standards files

--- scripts/myFirstExample.sh
+++ scripts/myFirstExample.sh
@@ -1,1 +1,18 @@
+#!/bin/bash
 
+#Comment: Testing script format
+#Date: 07-26-2022
+#Modification: 07-26-2022
+
+name='Camilo Astros'
+
+echo $name
+
+date
+df -h
+
+	if { $a -eq file }
+	then echo this
+	else
+	echo that
+	fi
